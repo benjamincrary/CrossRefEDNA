4f50e7d7b8717e893c31279dc1299270c887ff6f
benjamincrary <benjamin.crary@gmail.com>
2020-07-28 17:44:59 -0500

    Moved out of dev state

--- UpdateKnowledgeCenter.R
+++ UpdateKnowledgeCenter.R
@@ -1,5 +1,5 @@
 library(CrossRefEDNA)
 
-update_edna_knowledge_center("/var/www/vhosts/epri.com/edna/KnowledgeCenter/data/",
+update_edna_knowledge_center("/var/www/vhosts/epri.com/knowledge-center-input/",
                              "/var/www/vhosts/epri.com/edna/KnowledgeCenter/data/",
                              "bcrary@limno.com")
